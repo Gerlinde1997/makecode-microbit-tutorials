5098497536c60d015f4b45b1efc8118bee0d9c42
Gerlinde van Ginkel <60713279+Gerlinde1997@users.noreply.github.com>
2022-10-24 11:00:53 +0200

    update makecode microbit target

--- main.ts
+++ main.ts
@@ -1,14 +1,7 @@
-let snelheid = 100
 basic.forever(function () {
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, snelheid)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, snelheid)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, snelheid)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
+        maqueen.motorStop(maqueen.Motors.All)
     } else {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, snelheid)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
     }
 })
